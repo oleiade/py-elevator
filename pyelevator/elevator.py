4b62f6545dd962ad4b323454901ccf12a7ce9910
Theo Crevon <tcrevon@gmail.com>
2012-09-17 10:22:00 +0200

    Fix : range output datas format validation

--- pyelevator/elevator.py
+++ pyelevator/elevator.py
@@ -6,8 +6,8 @@ class RangeIter(object):
         self._container = range_datas if self._valid_range(range_datas) else None
 
     def _valid_range(self, range_datas):
-        if (not isinstance(range_datas, tuple) or
-            any(not isinstance(pair, tuple) for pair in range_datas)):
+        if (not isinstance(range_datas, (list, tuple)) or
+            any(not isinstance(pair, (list, tuple)) for pair in range_datas)):
             raise ValueError("Range datas format not recognized")
         return True
 
